90754e0088acfce4b4ac42a37709d9c1e85bb075
Jake Johnson <jkjohnson1996@gmail.com>
2024-02-01 17:17:51 -0800

    removed statistics controller and model, using player instead for brevity

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const statisticController = require('../controllers/statisticController');
 const gameLogController = require('../controllers/gameLogController');
 const playerController = require('../controllers/playerController');
 const superlativesController = require('../controllers/superlativesController');
 const router = express.Router();
-
-// Route to get all statistics
-router.get('/statistics', statisticController.getAllStatistics);
-
-// Route to create a new statistic entry - DON'T NEED YET
-// router.post('/statistics', statisticController.createStatistic);
-
-// Route to update a statistic entry - DON'T NEED YET
-// router.put('/statistics/:id', statisticController.updateStatistic);
-
-// Route to delete a statistic entry - DON'T NEED YET
-// router.delete('/statistics/:id', statisticController.deleteStatistic);
 
 // Route to get all game logs
 router.get('/game-log', gameLogController.getAllGameLogs);
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './style/Statistics.css';
 
 const Statistics = () => {
