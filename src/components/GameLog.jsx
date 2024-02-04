59163469cec967598f483d32b2df7d543900ada5
Jake Johnson <jkjohnson1996@gmail.com>
2024-02-04 00:40:42 -0800

    grouped all game log entries by date, added accordion toggle, things are looking good

--- src/components/GameLog.jsx
+++ src/components/GameLog.jsx
@@ -4,37 +4,50 @@ import './style/GameLog.css';
 
 const GameLog = () => {
     const [groupedGameLogs, setGroupedGameLogs] = useState({});
+    const [openGroups, setOpenGroups] = useState({});
 
     useEffect(() => {
         axios.get('/api/game-log')
             .then(response => {
                 const grouped = response.data.reduce((acc, log) => {
                     const date = log.date.slice(0, 10);
-                    // If the acc already has an array for that date, push the log into it, otherwise create a new array
                     !acc[date] ? acc[date] = [log] : acc[date].push(log);
                     return acc;
                 }, {});
 
                 setGroupedGameLogs(grouped);
+                // Initialize all groups as closed (false)
+                const initialOpenStates = Object.keys(grouped).reduce((acc, date) => {
+                    acc[date] = false; // Start with all groups collapsed
+                    return acc;
+                }, {});
+                setOpenGroups(initialOpenStates);
             })
             .catch(error => {
                 console.error("There was an error fetching the game logs: ", error);
             });
     }, []);
 
+    const toggleGroup = (date) => {
+        setOpenGroups(prevState => ({ ...prevState, [date]: !prevState[date] }));
+    };
+
     return (
         <div className='game-log-container'>
             <h2 className='game-log-heading'>Game Log</h2>
             {Object.entries(groupedGameLogs).map(([date, logs], index) => (
                 <div key={index} className='game-log-date-group'>
-                    <h3>{date.split('-').join('/').replace(/^(\d{4})\/(\d{2})\/(\d{2})$/, `$2/$3/$1`)}</h3>
-                    {logs.map((log, logIndex) => (
+                    <h3 onClick={() => toggleGroup(date)} className={openGroups[date] ? 'open' : ''}>
+                        {date.split('-').join('/').replace(/^(\d{4})\/(\d{2})\/(\d{2})$/, `$2/$3/$1`)}
+                        <span className="toggle-indicator">{openGroups[date] ? '▲' : '▼'}</span>
+                    </h3>
+                    {openGroups[date] && logs.map((log, logIndex) => (
                         <div key={logIndex} className='game-details-container'>
                             <div className='game-details'>
-                                <p><b>Winner:</b> {log.winner?.player} with {log.winner?.deck}</p>
-                                {log.secondPlace?.player && <p><b>2nd Place:</b> {log.secondPlace.player} with {log.secondPlace.deck}</p>}
-                                {log.thirdPlace?.player && <p><b>3rd Place:</b> {log.thirdPlace.player} with {log.thirdPlace.deck}</p>}
-                                {log.fourthPlace?.player && <p><b>4th Place:</b> {log.fourthPlace.player} with {log.fourthPlace.deck}</p>}
+                                <p><b>Winner:</b> {log.winner?.player} ● {log.winner?.deck}</p>
+                                {log.secondPlace?.player && <p><b>2nd Place:</b> {log.secondPlace.player} ● {log.secondPlace.deck}</p>}
+                                {log.thirdPlace?.player && <p><b>3rd Place:</b> {log.thirdPlace.player} ● {log.thirdPlace.deck}</p>}
+                                {log.fourthPlace?.player && <p><b>4th Place:</b> {log.fourthPlace.player} ● {log.fourthPlace.deck}</p>}
                                 {log.winningPlay && <p><b>Winning Play:</b> {log.winningPlay}</p>}
                                 {log.interestingPlays && <p><b>Interesting Plays:</b> {log.interestingPlays}</p>}
                                 {log.mvp && <p><b>MVP:</b> {log.mvp}</p>}
