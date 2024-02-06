828ba3342e150e0f24a78026216b85faf7571192
Jake Johnson <jkjohnson1996@gmail.com>
2024-02-05 22:27:58 -0800

    added login screen, changes routes accordingly, changed app top margin for styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import PlayerManagement from './components/PlayerManagement';
 import Statistics from './components/Statistics';
 import GameLog from './components/GameLog';
 import Superlatives from './components/Superlatives';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <div className="App">
-        <NavBar />
+        {isAuthenticated && <NavBar />}
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/player-management" element={<PlayerManagement />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route path="/game-log" element={<GameLog />} />
-          <Route path="/superlatives" element={<Superlatives />} />
+          <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Login onLogin={() => setIsAuthenticated(true)} />} />
         </Routes>
+        <div className='main-content'>
+          <Routes>
+            <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/" />} />
+            <Route path="/player-management" element={isAuthenticated ? <PlayerManagement /> : <Navigate to="/" />} />
+            <Route path="/statistics" element={isAuthenticated ? <Statistics /> : <Navigate to="/" />} />
+            <Route path="/game-log" element={isAuthenticated ? <GameLog /> : <Navigate to="/" />} />
+            <Route path="/superlatives" element={isAuthenticated ? <Superlatives /> : <Navigate to="/" />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
